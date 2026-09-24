19222159e3ebafcb5f97553f7f74d5a6bfb0a9b7
sqv1zyy <bulatov.ilya2018@yandex.ru>
2026-09-24 21:56:18 +0500

    Интегрировал TinyMCE и добавил конфигурацию AppConfig

---
+++ BuildService.Mvc.Api/BuildService.Mvc.Api/Program.cs
@@ -0,0 +1,15 @@
+namespace BuildService.Mvc.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    } 
+}
---
+++ BuildService.Mvc.Api/BuildService.Mvc.Api/infrastructure.cs
@@ -0,0 +1,17 @@
+﻿namespace BuildService.Mvc.Api
+{
+    public class AppConfig
+    {
+        public class TinyMce
+        {
+            public string? apiKey { get; set; }
+        }
+        public class Company
+        {
+            public string? CompanyName { get; set; }
+            public string? CompanyPhone { get; set; }
+            public string? CompanyPhoneShort { get; set; }
+            public string? CompanyEmail { get; set; }
+        }
+    }
+}
